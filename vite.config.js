001f0a7a8c41217d0e4f90f7257e471999360d80
Mattias89 <mattiassaeed89@gmail.com>
2024-05-25 15:33:15 +0200

    updated packages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copy } from 'vite-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'web.config', dest: 'dist' }
+      ]
+    })
+  ],
 })
